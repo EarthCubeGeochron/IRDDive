1c95e5ce26ec2ebcbc0025be3b234c0b29ac512e
SimonGoring <simon.j.goring@gmail.com>
2018-03-18 23:06:48 -0700

    Added a new function that can be called using `source`.  The function displays easy to use tables to examine matches, false matches and other information related to word matching in GDD.
    The general use is expected to be:
    
    ```
    subset <- sample(nrow(full_nlp), 100)
    
    nlp_summary(gddid = nlp$`_gddid`[subset], words = nlp$word[subset])
    ```
    
    Or similar.  This is intended to help streamline the process of examining the GDD output.

---
+++ R/browse.R
@@ -0,0 +1,92 @@
+#' @title Summarize publications
+#' @description A quick view method for looking at publication data from the GeoDeepDive publications.
+#' @param x One of, a vector of character \code{gddid}s from GeoDeepDive, or a \code{data.frame} with columns \code{gddid} and \code{words}.
+#' @param words A vector of sentences, similar to the GDD corpus \code{words} column.
+#' @param corpus The full GDD corpus of which \code{x} is expected to be a subset.
+#' @param pubs The bibliographic information for the GDD dataset.
+#' @importFrom dplyr left_join
+#' @example \dontrun{
+#' subset <- sample(nrow(full_nlp), 100)
+#' nlp_summary(gddid = nlp$`_gddid`[subset], words = nlp$word[subset])
+#' }
+#' @export
+
+nlp_summary <- function(x, corpus = NULL, pubs = NULL, words = NULL) {
+  
+  # Programming notes:
+  # I want to build this so we can pass in a data.frame either formatted as a corpus,
+  # returning the sentence, a publication ID and 
+  
+  clean_words <- function(x) {
+    gsub("[(${)(}^)]", "", x) %>%                # Curly brakets
+      gsub("(,([\\;\\:\\%]),)","\\2 ", .) %>%
+      gsub("(([^\"]),([^\"]))", "\\2 \\3", ., perl = TRUE)  %>% # This nees to be fixed.
+      gsub(",\",\",", ", ", .) %>%                 # Commas
+      gsub(" \\.", "\\.", .) %>% 
+      gsub("-LRB-\\s", "\\(", .) %>% 
+      gsub("(,-RRB-)|(\\s-RRB-)", "\\)", .)
+  }
+  
+  # gddid and sentences were passed in:
+  if (!is.data.frame(x) & !is.null(words) & is.null(pubs)) {
+    
+    # The only thing that's been passed in is the gddid and words.
+    assertthat::assert_that(is.character(words) | is.factor(words), 
+                            msg = "If you don't include the corpus the `words` element must be a character string. ")
+    assertthat::assert_that(length(words) == length(x),
+                            msg = "Both `words` and `x` must have the same length.")
+
+    output <- data.frame("gddid" = x,
+                         words = as.character(words) %>% clean_words)
+    
+  }
+  
+  if(!is.data.frame(x) & !is.null(pubs) & is.null(words) & is.null(corpus)) {
+    assertthat::assert_that(all(x %in% pubs$`_gddid`),
+                            msg = "There are unique identifiers passed that are not in the publication table.")
+    
+    colnames(pubs)[which(colnames(pubs) == "_gddid")] <- "gddid"
+    
+    pubs$doi <- paste0('<a href="http://dx.doi.org/', sapply(pubs$identifier, '[[', 'id'), '">DOI</a>')
+    
+    short_pub <- pubs[match(x, pubs$gddid),] %>% 
+      select(gddid, title, year, journal.name, doi)
+    
+    assertthat::assert_that(all(x == short_pub$gddid),
+                            msg = "There is a mismatch between publication and selected gddids.")
+    
+    output <- data.frame(short_pub)
+  }
+  
+  if(is.data.frame(x) & !is.null(pubs) & is.null(words) & is.null(corpus)) {
+
+    assertthat::assert_that(any(c('gddid', '_gddid') %in% colnames(x)),
+                 msg = "There must be a column either `gddid` or `_gddid` in `x` if `x` is a `data.frame`")
+    
+    assertthat::assert_that('words' %in% colnames(x),
+                            msg = "There must be a column `words` in `x` if `x` is a `data.frame`")
+
+    if ('_gddid' %in% colnames(x)) {
+      colnames(x)[which(colnames(x) == "_gddid")] <- "gddid"
+    }
+    
+    colnames(pubs)[which(colnames(pubs) == "_gddid")] <- "gddid"
+    
+    pubs$doi <- paste0('<a href="http://dx.doi.org/', sapply(pubs$identifier, '[[', 'id'), '">DOI</a>')
+    
+    short_pub <- dplyr::left_join(x, pubs, by = "gddid") %>% 
+      select(gddid, words, title, year, journal.name, doi)
+    
+    short_pub$words <- clean_words(short_pub$words)
+    
+    short_pub$gddid = paste0('<small><a title ="', 
+                             short_pub$gddid, '" href = "',
+                             short_pub$gddid,'">',
+                             substr(short_pub$gddid, 1, 2), '...', substr(short_pub$gddid, 20, 24),
+                             '</span></small>')
+    
+    output <- data.frame(short_pub)
+  }
+  
+  return(DT::datatable(output, escape = FALSE, rownames = FALSE))
+}
