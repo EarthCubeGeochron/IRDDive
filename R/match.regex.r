81b4dbd45416c443365203de7e94ddae77c4dcd5
jmarsicek <jmarsicek@wisc.edu>
2018-12-06 13:29:45 -0600

    R code to look for regex in the papers, identify the papers, and generate vectors of distance values of ages (for example) to coordinate pairs in a paper.

---
+++ R/match.regex.r
@@ -0,0 +1,19 @@
+#' @title Match regular expressions and words and return a vector of T/F.
+#' @param regEx The input regular expression
+#' @param df The input data frame with a column named "word" that contains words from literatures
+#' 
+
+#df = full_nlp
+#regEx <- "[\\{,][-]?[1]?[0-9]{1,2}\\.[0-9]{1,}[,]?[NESWnesw],"
+match.regex <- function(regEx, df){
+  detectVector <- vector()
+  for(i in 1:nrow(df)){
+    detect <- str_detect(df$word[i], regEx)
+    if(detect){
+      detectVector <- c(detectVector, TRUE)
+    } else {
+      detectVector <- c(detectVector, FALSE)
+    }
+  }
+}
+return(detectVector)
